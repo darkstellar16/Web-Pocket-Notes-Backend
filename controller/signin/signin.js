131590f8aa96be728afbb1e1c7e0671356ee4423
Shubham <itsmeshubham2799@gmail.com>
2024-02-21 18:45:09 +0530

    changes

--- controller/signin/signin.js
+++ controller/signin/signin.js
@@ -1,31 +1,31 @@
 require("dotenv").config();
 
 const signupInfo = require("../../models/userModel.js");
-
 const jwt = require("jsonwebtoken");
-
 const bcrypt = require("bcrypt");
 
 const signinData = async (req, res) => {
     try {
-
         const { email, password } = req.body;
-        if (!(email && password)) {
-            return res.status(404).send("Fields are required");
-        }
-        const user = await signupInfo.findOne({ email });
-        if (user && (await bcrypt.compare(password, user.password))) {
 
-            const token = jwt.sign({ email, password }, process.env.SECRET_KEY, { expiresIn: "4h" })
+        if (!(email && password)) {
+            return res.status(404).json({ status: 404, message: "Fields are required" });
+        }
+
+        const user = await signupInfo.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign({ email, password }, process.env.SECRET_KEY, { expiresIn: "4h" });
             user.token = token;
 
-            return res.status(200).json({ user });
-        }
-        else {
-            res.status(400).send("invalid credentials")
+            return res.status(200).json({ status: 200, message: "Login successful", user });
+        } else {
+            res.status(400).json({ status: 400, message: "Invalid credentials" });
         }
     } catch (error) {
         console.log(error);
+        res.status(500).json({ status: 500, message: "Internal Server Error" });
     }
 }
+
 module.exports = signinData;
--- controller/signup/signup.js
+++ controller/signup/signup.js
@@ -8,13 +8,14 @@ const signupData = async (req, res) => {
         const { email, password, username } = req.body;
 
         if (!(email && password)) {
-            return res.status(400).send("Please Input the mentioned details");   //Asking for details
+            return res.status(400).json({ status: 400, message: "Please Input the mentioned details" });   //Asking for details
         }
 
         const oldUser = await signupInfo.findOne({ email });
 
         if (oldUser) {
-            return res.status(409).send("Please login you are already registered user");  //checking whether the user is already present or not..
+            return res.status(409).json({ status: 409, message: "Please login, you are already registered" });
+            //checking whether the user is already present or not..
         }
 
         const encryptedPasword = await bcrypt.hash(password, 10);
@@ -24,10 +25,11 @@ const signupData = async (req, res) => {
             email: email,
             password: encryptedPasword
         })
-        res.status(200).send("User have registered successfully");
+        res.status(200).json({ status: 200, message: "User has registered successfully" });
     }
     catch (error) {
         console.log(error);
+        res.status(500).json({ status: 500, message: "Internal Server Error" });
     }
 }
 
